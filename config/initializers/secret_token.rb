27123fad3e70654f20032f7683d77c38ec5f0e91
dongkwan <insinoa@gmail.com>
2013-01-03 11:22:20 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GMS::Application.config.secret_token = '0abe39e393ce0dc3b79a8f3ca0cfad443213342c626c1c8fbdc145142f3a35c4cbdd3131f192f10d9b34fee9533451838512c1d9d01c42c08fb73b7be79eefe8'
